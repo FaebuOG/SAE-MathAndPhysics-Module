fe7cc2c26d247d29f5f4b54e536011e07a98d1c0
FaebuOG <soly.fabian.g@gmail.com>
2022-06-22 01:15:53 +0200

    Cellular Automata test commit

--- Assets/1_Mathematisches Regelwerk/Scripts/MapGenerator.cs
+++ Assets/1_Mathematisches Regelwerk/Scripts/MapGenerator.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
@@ -29,7 +27,7 @@ public class MapGenerator : MonoBehaviour
 
     void GenerateMap()
     {
-        map = new int[width, height];
+        map = new int[width, height]; // Initialisiert dem 2D array eine Breite und Höhe
         RandomFillMap();
 
         for (int i = 0; i < 5; i++)
@@ -51,9 +49,10 @@ public class MapGenerator : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                if (x == 0 || x == width-1 || y == 0 || y == height-1)
                 {
-                    map[x, y] = 1;
+                    map[x, y] = 1; // ?
+                    Debug.Log(map);
                 }
                 else
                 {
@@ -70,42 +69,62 @@ public class MapGenerator : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                int neighbourWallTiles = GetSurroundingWallCount(x, y);
+                int neighbourWallTiles = GetSurroundingWallCount(x,y); // -> Gibt die Koordinate zur richtigen Zelle an.
 
+                Debug.Log("x = " + x);
+                Debug.Log("y = " + y);
+                // Wenn die Zelle mehr als 4 Nachbarn hat, soll sie zum Leben erweckt werden.
                 if (neighbourWallTiles > 4)
                     map[x, y] = 1;
-                else if(neighbourWallTiles < 4)
-                {
+                else if(neighbourWallTiles < 4) // Sonst hat sie halt Pech gehabt.
                     map[x, y] = 0;
-                }
             }
         }
     }
 
-    
-    int GetSurroundingWallCount(int gridX, int gridY)
-    {
+   //int GetSurroundingWallCount(int gridX, int gridY)
+   //{
+   //    int wallCount = 0;
+   //    // Loops trough a 3x3 grid -> gridX and gridY
+   //    for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
+   //    {
+   //        for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
+   //        {
+   //            // Check if grid is inside the map
+   //            if (neighbourX >= 0 && neighbourX <= width && neighbourY >= 0 && neighbourY <= width)
+   //            {
+   //                if (neighbourX != gridX || neighbourY != gridY)
+   //                {
+   //                    wallCount += map[neighbourX, neighbourY];
+   //                }
+   //            }
+   //            else // if grid is outside of the map
+   //            {
+   //                wallCount += 1;
+   //            }
+   //        } 
+   //    }
+   //   return wallCount;
+   //}
+   
+    int GetSurroundingWallCount(int x, int y){
         int wallCount = 0;
-        // Loops trough a 3x3 grid -> gridX and gridY
-        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
-        {
-            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
-            {
-                // Check if grid is inside the map
-                if (neighbourX >= 0 && neighbourX <= width && neighbourY >= 0 && neighbourY <= width)
-                {
-                    if (neighbourX != gridX || neighbourY != gridY)
-                    {
-                        wallCount += map[neighbourX, neighbourY];
-                    }
-                }
-                else // if grid is outside of the map
-                {
-                    wallCount += 1;
-                }
-                
-            } 
-        }
+
+        // Wenn die Zelle nicht ganz oben auf der Map ist.
+        // -> Checke die Zelle über der aktuellen Zelle.
+        if(y!=0){wallCount += map[x,y-1];}
+
+        // Wenn die Zelle nicht ganz unten auf der Map ist.
+        // -> Checke die Zelle unter der aktuellen Zelle.
+        if(y!=height){wallCount += map[x,y+1];}
+
+        // Wenn die Zelle nicht ganz links auf der Map ist.
+        // -> Checke die Zelle links von der aktuellen Zelle.
+        if(x!=0){wallCount += map[x-1,y];}
+
+        // Wenn die Zelle nicht ganz rechts auf der Map ist.
+        // -> Checke die Zelle rechts von der aktuellen Zelle.
+        if(x!=height){wallCount += map[x+1,y];}
 
         return wallCount;
     }
@@ -118,9 +137,9 @@ public class MapGenerator : MonoBehaviour
             for (int y = 0; y < height; y++)
             {
                 // checks if the result of the values in the 2D array was 1 or 0 and gives the right color.
-                Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+                Gizmos.color = (map[x, y] == 1)? Color.black : Color.white;
 
-                Vector3 pos = new Vector3(-width / 2 + x + 0.5f, 0, -height / 2 + y + 0.5f);
+                Vector3 pos = new Vector3(-width/2 + x + 0.5f, 0, -height/2 + y + 0.5f);
 
                 Gizmos.DrawCube(pos, Vector3.one);
                 //Instantiate(cubePrefab, pos, Quaternion.identity);
