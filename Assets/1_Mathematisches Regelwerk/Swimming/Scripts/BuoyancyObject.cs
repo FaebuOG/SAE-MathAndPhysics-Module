bcba856ea06760e0c410d509a33d9b8ca8acb1a5
FaebuOG <soly.fabian.g@gmail.com>
2022-06-28 01:13:59 +0200

    Swimming mechanic

---
+++ Assets/1_Mathematisches Regelwerk/Swimming/Scripts/BuoyancyObject.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BuoyancyObject : MonoBehaviour
+{
+
+    [SerializeField] private Transform[] floaters;
+    public float underWaterDrag = 3f;
+    public float underWaterAngularDrag = 1f;
+    
+    public float airDrag = 0f;
+    public float airAngularDrag = 0.05f;
+
+    public float floatingPower = 15f;
+    private Rigidbody rigidbody;
+
+    public float waterHeight = 0;
+    private int floatersUnderWater;
+    private bool underWater;
+
+    
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        floatersUnderWater = 0;
+        for (int i = 0; i < floaters.Length; i++)
+        {
+            float difference = floaters[i].position.y - waterHeight;
+            
+            if (difference < 0)
+            {
+                rigidbody.AddForceAtPosition(Vector3.up * floatingPower * Mathf.Abs(difference), floaters[i].position, ForceMode.Force);
+                floatersUnderWater += 1;
+                if (!underWater)
+                {
+                    underWater = true;
+                    SwitchState(true);
+                }
+            }
+            
+            if(underWater && floatersUnderWater == 0)
+            {
+                underWater = false;
+                SwitchState(false);
+            }
+        }
+        
+
+        
+
+        void SwitchState(bool isUnderwater)
+        {
+            if (isUnderwater)
+            {
+                rigidbody.drag = underWaterDrag;
+                rigidbody.angularDrag = underWaterAngularDrag;
+            }
+            else
+            {
+                rigidbody.drag = airDrag;
+                rigidbody.angularDrag = airAngularDrag;
+            }
+            
+        }
+        
+    }
+}
