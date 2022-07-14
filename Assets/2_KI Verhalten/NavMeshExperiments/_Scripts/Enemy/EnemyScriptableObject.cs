e696d61441080f32dcd29e9af92abd674a232e0a
FaebuOG <soly.fabian.g@gmail.com>
2022-07-14 16:38:27 +0200

    attacking system

--- Assets/2_KI Verhalten/NavMeshExperiments/_Scripts/Enemy/EnemyScriptableObject.cs
+++ Assets/2_KI Verhalten/NavMeshExperiments/_Scripts/Enemy/EnemyScriptableObject.cs
@@ -6,12 +6,15 @@ using UnityEngine.AI;
 [CreateAssetMenu (fileName = "Enemy Configuration", menuName = "ScriptableObjects/Enemy Configuration")]
 public class EnemyScriptableObject : ScriptableObject
 {
-    // Enemy stats
+    // Enemy Stats
     public int Health = 100;
+    public float AttackDelay = 1f;
+    public int Damage = 5;
+    public float AttackRadius = 1.5f;
 
-    // Nav Mesh Agent 
+    // NavMeshAgent Configs
     public float AIUpdateInterval = 0.1f;
-    
+
     public float Acceleration = 8;
     public float AngularSpeed = 120;
     // -1 means everything
---
+++ Assets/2_KI Verhalten/NavMeshExperiments/_Scripts/Player/PlayerCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject followTarget;
+    [SerializeField]
+    private Camera camera;
+    [SerializeField]
+    private Vector3 offset;
+
+    private void Update()
+    {
+        camera.transform.position = followTarget.transform.position + offset;
+    }
+}
