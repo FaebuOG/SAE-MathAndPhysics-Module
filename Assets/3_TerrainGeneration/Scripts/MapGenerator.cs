8fae7a9fd72e9cc2c86a1d922b96a9c35af54ab6
FaebuOG <soly.fabian.g@gmail.com>
2022-07-18 12:48:23 +0200

    Octaves

--- Assets/3_TerrainGeneration/Scripts/MapGenerator.cs
+++ Assets/3_TerrainGeneration/Scripts/MapGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,13 +9,36 @@ public class MapGenerator : MonoBehaviour
     [Range(0,100)] public int MapWidth;
     [Range(0,100)] public int MapHeight;
     [Range(0,100)] public float NoiseScale;
+    
+    [Range(0,  1)] public float Persistance;
+    public float Lacunarity;
+    public int Octaves;
+    public int Seed;
+    public Vector2 Offset;
     public bool AutoUpdate;
+
+    
     
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(MapWidth, MapHeight, NoiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(MapWidth, MapHeight, Seed, NoiseScale, Octaves, Persistance, Lacunarity, Offset);
 
         MapDisplay mapDisplay = FindObjectOfType<MapDisplay>();
         mapDisplay.DrawNoiseMap(noiseMap);
+    }
+
+    private void OnValidate()
+    {
+        if (MapWidth < 1)
+            MapWidth = 1;
+        
+        if (MapHeight < 1)
+            MapHeight = 1;
+
+        if (Lacunarity < 1)
+            Lacunarity = 1;
+
+        if (Octaves < 0)
+            Octaves = 0;
     }
 }
